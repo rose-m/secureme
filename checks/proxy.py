74be3b5fdfc7c3929bc09cb3bb4d731ea47c1f79
rosem <michael_rose@gmx.de>
2018-10-20 15:43:47 +0200

    check certificates for google.com (with fingerprints)

--- checks/proxy.py
+++ checks/proxy.py
@@ -1,12 +1,35 @@
+from collections import namedtuple
+from socket import socket
 from threading import Timer
+from typing import List, Optional
 from urllib import request
 
+from OpenSSL import SSL
+from OpenSSL.crypto import X509
 from bs4 import BeautifulSoup
 
 from base.module import Module
+from base.status import CheckStatus
+
+Fingerprint = namedtuple(typename="Fingerprint",
+                         field_names=["name", "sha1"])
+
+
+def _get_ssl_cert() -> Optional[List[X509]]:
+    with socket() as sock:
+        sock.connect(("www.google.com", 443))
+        ctx = SSL.Context(SSL.SSLv23_METHOD)
+        conn = SSL.Connection(ctx, sock)
+        conn.set_connect_state()
+        conn.do_handshake()
+
+        cert_chain = conn.get_peer_cert_chain()
+        conn.close()
+        return cert_chain
 
 
 class ProxyCheck(Module):
+    _fingerprints: List[Fingerprint]
     _periodic_timer: Timer
 
     PROXY_CHECK_INTERVAL = 15
@@ -15,14 +38,13 @@ class ProxyCheck(Module):
         super(ProxyCheck, self).__init__()
 
         self._periodic_timer = None
+        self._fingerprints = []
 
     def activate(self):
-        self._initialize_fingerprints()
         self._do_periodic_check()
-        self._setup_timer()
 
     def deactivate(self):
-        pass
+        self._reset_timer()
 
     def _setup_timer(self):
         self._reset_timer()
@@ -36,7 +58,42 @@ class ProxyCheck(Module):
             self._periodic_timer = None
 
     def _do_periodic_check(self):
-        print("check")
+        if len(self._fingerprints) == 0:
+            self._status = CheckStatus.WARN
+            self._initialize_fingerprints()
+        else:
+            self._check_certificate()
+
+        print(">> ProxyCheck: %s" % self._status)
+        self._setup_timer()
+
+    def _check_certificate(self):
+        certs: Optional[List[X509]] = None
+        try:
+            certs = _get_ssl_cert()
+        except Exception as e:
+            print("Error: %s" % e)
+
+        if certs is None or len(certs) == 0:
+            self._status = CheckStatus.ALERT
+            return
+
+        site_cert = certs[0]
+        site_subject = site_cert.get_subject()
+        cns = [v for n, v in site_subject.get_components() if n == b'CN']
+        if len(cns) > 0 and cns[0] != b'www.google.com':
+            self._status = CheckStatus.ALERT
+            return
+
+        cert_shas = [(cert, str(cert.digest("sha1"))[2:-1].lower()) for cert in certs]
+        for cert, sha in cert_shas:
+            for fp in self._fingerprints:
+                if fp.sha1 == sha:
+                    print("Matching: %s - %s" % (fp.name, fp.sha1))
+                    self._status = CheckStatus.OK
+                    return
+
+        self._status = CheckStatus.ALERT
 
     def _initialize_fingerprints(self):
         def beauty(content):
@@ -51,6 +108,6 @@ class ProxyCheck(Module):
                 tds = r.select("td")
                 name = tds[0].getText().strip()
                 sha = tds[2].getText().strip()
-                fingerprints.append((name, sha))
+                fingerprints.append(Fingerprint(name=name, sha1=sha))
 
-            print(fingerprints)
+            self._fingerprints = fingerprints
